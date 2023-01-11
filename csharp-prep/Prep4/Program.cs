c69d96754ee774632f7216c49af0286d38a6afd8
Kimball Christensen <kimballchristensen@gmail.com>
2023-01-10 19:15:12 -0700

    List of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        bool repeat = true;
+        while (repeat == true)
+        {
+            Console.Write("Enter an integer to be added to the list (enter '0' when done): ");
+            string userNum = Console.ReadLine();
+            int userAns = int.Parse(userNum);
+
+            if (userAns == 0)
+            {
+                Console.WriteLine("List finished.");
+                repeat = false;
+            }
+            else
+            {
+                numbers.Add(userAns);
+            }
+        }
+
+        Console.WriteLine("Here's the list:");
+        Console.WriteLine("");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine("");
+
+        int sum = numbers.Sum();
+        int avg = (sum) / (numbers.Count());
+        int max = numbers.Max();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {avg}");
+        Console.WriteLine($"Maximum: {max}");
+        Console.WriteLine("");
     }
 }
