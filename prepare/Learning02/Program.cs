449650a0401a5f52e23d61b5286eec8db9472a17
Kimball <kimballchristensen@gmail.com>
2023-01-16 14:50:39 -0700

    Created classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Walmart";
+        job1._startYear = 2010;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "SoftWare Engineer";
+        job2._companyName = "Sony";
+        job2._startYear = 2015;
+        job2._endYear = 2021;
+
+        Resume resume1 = new Resume();
+        resume1._userName = "Kimball Christensen";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
