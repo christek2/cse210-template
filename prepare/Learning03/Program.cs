63aef1024c5bb273377e67cb46fb6acb61f93d3f
Kimball <kimballchristensen@gmail.com>
2023-02-02 18:09:24 -0700

    Encapsulation activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        f1.SetDenominator(3);
+        Console.WriteLine(f1.GetDenominator());
+
+        f2.SetNumerator(3);
+        f2.SetDenominator(4);
+        Console.WriteLine(f2.GetDecimalValue());
     }
 }
