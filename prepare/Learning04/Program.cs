cd60cb742c65d73fc932dbaafa67895be1c7efa7
Kimball <kimballchristensen@gmail.com>
2023-02-16 12:31:01 -0700

    Added the classes and practiced inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment test = new Assignment("Kimball", "Accounting");
+       Console.WriteLine(test.GetSummary());
+
+       MathAssignment math1 = new MathAssignment("Kimball", "Accounting", "10", "1-12");
+       Console.WriteLine(math1.GetSummary());
+       Console.WriteLine(math1.GetHomeworkList());
+
+       WritingAssignment write1 = new WritingAssignment("Kimball", "History", "World History");
+       Console.WriteLine(write1.GetSummary());
+       Console.WriteLine(write1.GetWritingInformation());
+
     }
 }
