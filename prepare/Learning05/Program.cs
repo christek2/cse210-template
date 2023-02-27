7b01367b88e88db6ab5a33df34526eed50e4859a
Kimball <kimballchristensen@gmail.com>
2023-02-27 14:42:52 -0700

    Completed the polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Console.WriteLine(square.GetArea());
+        shapeList.Add(square);
+
+        Rectangle rect = new Rectangle("blue", 5, 2);
+        Console.WriteLine(rect.GetArea());
+        shapeList.Add(rect);
+
+        Circle circle = new Circle("green", 3);
+        Console.WriteLine(circle.GetArea());
+        shapeList.Add(circle);
+
+        foreach (Shape shape in shapeList)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+        
     }
 }
