8a74b39fc0e4d506e7e3d40fb1c0ac62f1427554
Kimball <kimballchristensen@gmail.com>
2023-03-22 13:55:38 -0600

    Worked on Menu class more

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -13,19 +13,43 @@ public class Menu
         _options.Add("1. Add Asset");
         _options.Add("2. Remove Asset");
         _options.Add("3. Edit Existing Asset's Information");
+        _options.Add("4. Back");
     }
 
     public void DisplayManageOptions()
     {
         Console.Clear();
+        Manage mng = new Manage();
 
         foreach (string option in _options)
         {
             Console.WriteLine(option);
         }
         Console.WriteLine();
-        Console.Write("Choose an option above: ");
-        int userInput = Console.Read();
+        Console.Write("Enter an option from the list: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        switch (userInput)
+        {
+            case 1:
+                mng.AddAssets();
+                break;
+            case 2:
+                mng.RemoveAssets();
+                break;
+            case 3:
+                mng.EditAssets();
+                break;
+            case 4:
+                // ShowAnimation();
+                break;
+            case 5:
+                Console.WriteLine("Seletion not available; please enter an option from the list above. ");
+                ShowAnimation();
+                DisplayManageOptions();
+                break;
+        }
+        DisplayInterface();
     }
     public string DisplayInterface()
     {
