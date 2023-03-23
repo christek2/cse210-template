183a1c6e98c828473158111908ae9c620f4eba6f
Kimball <kimballchristensen@gmail.com>
2023-03-23 15:58:59 -0600

    Worked on removing

--- final/FinalProject/Manage.cs
+++ final/FinalProject/Manage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 public class Manage : Depr
 {
@@ -24,7 +25,7 @@ public class Manage : Depr
         string date = Console.ReadLine();
         Console.Write("What was the intial cost of the asset (exclude symbols and commas)? ");
         double cost = double.Parse(Console.ReadLine());
-        Console.Write("What is the expected salvage value of the asset? ");
+        Console.Write("What is the expected salvage value of the asset (exclude symbols and commas)? ");
         double salvage = double.Parse(Console.ReadLine());
 
         string method = "";
@@ -54,8 +55,10 @@ public class Manage : Depr
 
         if (userInput == "yes")
         {
-            Save save = new Save();
-            save.SaveToFile(_fileName);
+            string newAsset = $"{method}; {name}; {desc}; {date}; {cost}; {salvage}";
+            PrintToSheet(_fileName, newAsset);
+            Console.WriteLine($"Saving ");
+            ShowAnimation();
         }
         else if (userInput == "no")
         {
@@ -77,7 +80,53 @@ public class Manage : Depr
         }
     }
     public void RemoveAssets()
-    {}
+    {
+        Console.Clear();
+
+        string[] assets = System.IO.File.ReadAllLines(_fileName);
+        int index = 1;
+        foreach (string i in assets)
+        {
+            Console.WriteLine($"{index}. {i}");
+            index = index + 1;
+        }
+        Console.WriteLine();
+        Console.Write("Enter the number of the asset you would like to delete: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        Console.Clear();
+        Console.WriteLine(assets[userInput - 1]);
+        Console.WriteLine();
+        Console.Write("Is this the item you would like to delete (yes/no)? ");
+        string userInput2 = Console.ReadLine();
+
+        if (userInput2 == "yes")
+        {
+            List<string> revisedAssets = new List<string>();
+            foreach (string i in assets)
+            {
+                revisedAssets.Add(i);
+            }
+            revisedAssets.RemoveAt(userInput - 1);
+            using (StreamWriter file = new StreamWriter(_fileName))
+            {
+                foreach (string i in revisedAssets)
+                {
+                    file.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Removing ");
+            ShowAnimation();
+        }
+        else if (userInput2 == "no")
+        {
+            RemoveAssets();
+        }
+        else
+        {
+            DisplayManageOptions();
+        }
+    }
     public void EditAssets()
     {}
     public int GetPosition()
@@ -96,6 +145,25 @@ public class Manage : Depr
     {
         _fileName = fileName;
     }
-    public void PrintToSheet()
-    {}
+    public void PrintToSheet(string fileName, string newAsset)
+    {
+        List<string> savedAssets = new List<string>();
+        string[] assets = System.IO.File.ReadAllLines(fileName);
+        int newLineIndex = assets.Count();
+
+        foreach (string asset in assets)
+        {
+            savedAssets.Add(asset);
+        }
+
+        savedAssets.Insert(newLineIndex, newAsset);
+
+        using (StreamWriter file = new StreamWriter(fileName))
+        {
+            foreach (string line in savedAssets)
+            {
+                file.WriteLine(line);
+            }
+        }
+    }
 }
